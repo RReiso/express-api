60bb0cbc32382410c47bed73e288a5ed9d3413b1
Ruta Reisoglu <rutareiso@protonmail.com>
2022-02-03 23:55:26 -0500

    Change directory name

--- api/routes/recipes.js
+++ api/routes/recipes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.route("/").get((req, res) => {
-  res.send("hello");
+  res.send("All recipes");
 });
 
 module.exports = router;
