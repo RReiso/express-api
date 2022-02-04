aadf6e2c312c824734b89396de1e914a41e48d2e
Ruta Reisoglu <rutareiso@protonmail.com>
2022-02-04 00:13:29 -0500

    Set up get route for recipes, send recipe names

--- api/routes/recipes.js
+++ api/routes/recipes.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const recipeData = require("../db/data.json");
 const router = express.Router();
 
 router.route("/").get((req, res) => {
-  res.send("All recipes");
+  const responseData = { recipeNames: [] };
+  recipeData.recipes.forEach((recipe) =>
+    responseData.recipeNames.push(recipe.name)
+  );
+  res.status(200).send(responseData);
 });
 
 module.exports = router;
