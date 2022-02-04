ec8534cf9f6a8e16bf8b0cb56fa20655e933bda7
Ruta Reisoglu <rutareiso@protonmail.com>
2022-02-04 10:12:45 -0500

    Add a GET route for single recipe

--- api/app.js
+++ api/app.js
@@ -6,9 +6,9 @@ const PORT = 8079;
 app.use(morgan("dev"));
 // app.use(express.json());
 
-const recipesRoutes = require("./routes/recipes");
-app.use("/recipes", recipesRoutes);
+const recipeRoutes = require("./routes/recipes");
+app.use("/recipes", recipeRoutes);
 
 app.listen(PORT, () => {
-  console.log(`App is listening on port ${PORT}`);
+  console.log(`app is listening on port ${PORT}`);
 });
--- api/routes/recipes.js
+++ api/routes/recipes.js
@@ -1,12 +1,25 @@
 const express = require("express");
-const recipeData = require("../db/data.json");
 const router = express.Router();
+const data = require("../db/data.json");
 
 router.route("/").get((req, res) => {
   const responseData = { recipeNames: [] };
-  recipeData.recipes.forEach((recipe) =>
-    responseData.recipeNames.push(recipe.name)
-  );
+  for (recipe of data.recipes) {
+    responseData.recipeNames.push(recipe.name);
+  }
+  res.status(200).send(responseData);
+});
+
+router.route("/details/:recipeName").get((req, res) => {
+  const recipeName = req.params.recipeName;
+  const responseData = {};
+
+  for (recipe of data.recipes) {
+    if (recipe.name === recipeName) {
+      responseData.details = { ingredients: [...recipe.ingredients] };
+      responseData.details.numsteps = recipe.instructions.length;
+    }
+  }
   res.status(200).send(responseData);
 });
 
