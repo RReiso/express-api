55929f5610edd030fa3feb392fc2da6feaed5fe1
Ruta Reisoglu <rutareiso@protonmail.com>
2022-02-03 23:43:14 -0500

    Initial setup

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const morgan = require("morgan");
+const express = require("express");
+const app = express();
+const PORT = 8079;
+
+app.use(morgan("dev"));
+app.use(express.json());
+
+const recipesRoutes = require("./routes/recipes");
+app.use("/recipes", recipesRoutes);
+
+app.listen(PORT, () => {
+  console.log(`App is listening on port ${PORT}`);
+});
